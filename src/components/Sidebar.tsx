64615611f3234febafaf939a945a7ccd434aa76a
Bablu Ahmed <bablukpik@gmail.com>
2025-03-25 03:52:17 +0600

    rc_2.0.0: fix sidebar style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,22 @@
-"use client";
+'use client';
 
 import { usePathname, useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 
 const Sidebar = () => {
   const router = useRouter();
-  const currentPath = usePathname()
+  const currentPath = usePathname();
 
   return (
-    <aside className="w-64 h-full bg-gray-900 text-white p-4 shadow-lg border-r border-gray-700">
-      <h2 className="text-lg font-bold mb-4">Navigation</h2>
-      <ul className="space-y-2">
+    <aside className='w-64 bg-gray-900 text-white p-4 shadow-lg border-r border-gray-700'>
+      <h2 className='text-lg font-bold mb-4'>Navigation</h2>
+      <ul className='space-y-2'>
         <li>
           <Button
-            variant="link"
-            className={`w-full text-left ${currentPath === '/dashboard' ? 'text-blue-400' : 'text-gray-200'} hover:bg-gray-700 rounded-md p-2`}
+            variant='link'
+            className={`w-full text-left ${
+              currentPath === '/dashboard' ? 'text-blue-400' : 'text-gray-200'
+            } hover:bg-gray-700 rounded-md p-2`}
             onClick={() => router.push('/dashboard')}
           >
             Dashboard
@@ -22,8 +24,10 @@ const Sidebar = () => {
         </li>
         <li>
           <Button
-            variant="link"
-            className={`w-full text-left ${currentPath === '/users' ? 'text-blue-400' : 'text-gray-200'} hover:bg-gray-700 rounded-md p-2`}
+            variant='link'
+            className={`w-full text-left ${
+              currentPath === '/users' ? 'text-blue-400' : 'text-gray-200'
+            } hover:bg-gray-700 rounded-md p-2`}
             onClick={() => router.push('/users')}
           >
             User Management
@@ -35,4 +39,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar; 
+export default Sidebar;
